50f3e7c2f4d5d26b56ede23812f1c74585e3a0b3
Cleyfson <cleyfson12@outlook.com>
2022-02-16 19:37:53 -0300

    Estilizando projeto blog pessoal usando React

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -8,8 +8,8 @@ function Home(){
        <Grid   className="back">
                 <Grid  item xs={6}>
                     <Box paddingX={20} className="text">
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "black", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "black", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "#054F77", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#054F77", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
